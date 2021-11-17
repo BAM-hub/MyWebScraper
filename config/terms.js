6e62ffe5bb92fc07734b9a41752c55b9dca06164
BAM-hub <bsharamin12@gmail.com>
2021-11-17 22:31:56 +0200

    variables holder

---
+++ config/terms.js
@@ -0,0 +1,18 @@
+const notNeededLinks = [
+  'Terms of use',
+  'Privacy Policy',
+  'Cookie Policy',
+  'Customer service',
+  'FAQ', 'English',
+  'Español', 'Français',
+  'Dansk', 'Norsk Bokmål',
+  'Svenska', 'Nederlands',
+  'Italiano', 'Suomi',
+  'Deutsch', 'Русский',
+  'Indonesia', 'Türkçe',
+  'Polski', 'Čeština',
+  'Melayu', 'Ελληνικά',
+  'Português'
+];
+
+module.exports = notNeededLinks;
